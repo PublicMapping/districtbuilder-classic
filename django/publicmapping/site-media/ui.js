b0bb9ab4cf16c38aa4e55b393983fd7b69a35a69
Brian Jacobs <bjacobs@azavea.com>
2010-07-16 17:04:03 +0000

    map layout update

--- django/publicmapping/site-media/ui.js
+++ django/publicmapping/site-media/ui.js
@@ -1,8 +1,26 @@
 $(function() {
 		$('#steps').tabs();
     
-    $('#mapmenuheader').click(function(){
-        $('#mapmenucontent').slideToggle(200);
+    $("button").button();
+    
+    $(".menu_toggle")
+        .button({
+          icons: {primary: 'ui-icon-triangle-1-s'},text: false})
+        .toggle(
+          function(){
+            $(this).button({icons: {primary: 'ui-icon-triangle-1-e'}})
+          },
+          function(){
+            $(this).button({icons: {primary: 'ui-icon-triangle-1-s'}})
+          })
+        .click(function(){
+          $('.map_menu_content').slideToggle(200);
+        });
+
+    $('.toolbar_toggle').click(function(){
+        $currtoolset = $(this).parent();
+        $currtoolset.hide();
+        $('.toolset').not($currtoolset).show()
     })
     $("button").button();
 
