8b143173f2b01dfec1714bc276f270dac800ad50
Brian Jacobs <bjacobs@azavea.com>
2010-07-16 00:18:52 +0000

    map layout updates

--- django/publicmapping/site-media/ui.js
+++ django/publicmapping/site-media/ui.js
@@ -4,7 +4,13 @@ $(function() {
     $('#mapmenuheader').click(function(){
         $('#mapmenucontent').slideToggle(200);
     })
-    
+    $("button").button();
+
+    $('.toolbar_toggle').click(function(){
+        $currtoolset = $(this).parent();
+        $currtoolset.hide();
+        $('.toolset').not($currtoolset).show()
+    })
     
     
 	});
