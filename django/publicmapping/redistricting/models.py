0692e6ea6d1ac19db761d8320f51b5569d7dd4cf
David Zwarg <dzwarg@azavea.com>
2010-07-19 15:59:44 +0000

    Updated add_geounits method to operate on large geography, and not bazillions of base geounit geometries.

--- django/publicmapping/redistricting/models.py
+++ django/publicmapping/redistricting/models.py
@@ -33,12 +33,24 @@ class Geounit(models.Model):
 
     @staticmethod
     def get_base_geounits(geounit_ids, geolevel):
-        new_geounit_ids = []
-        for geounit_id in geounit_ids:
-            target = Geounit.objects.get(pk=geounit_id).geom
-            base_geounits = Geounit.objects.filter(geom__within=target, geolevel=settings.BASE_GEOLEVEL).values_list('id', flat=True)
-            new_geounit_ids.extend(base_geounits)
-        return new_geounit_ids
+        targets = Geounit.objects.filter(id__in=geounit_ids)
+        if len(targets) > 0:
+            whole_geom = targets[0].geom
+            if len(targets) > 1:
+                for i in range(1,len(targets)):
+                    for target_geom in targets[i].geom:
+                        whole_geom.append(target_geom)
+
+        else:
+            return []
+
+        qs = Geounit.objects.filter(geom__within=whole_geom, geolevel=settings.BASE_GEOLEVEL)
+
+        # this is frustrating!  this forces the DB query to executue
+        # but it would be awesome to use these IDs as a subquery filter
+        # instead of fetching the values and sending them back again
+        # this is a django bug, I suspect (dz)
+        return list(qs.values_list('id', flat=True))
 
     def __unicode__(self):
         return self.name
@@ -81,6 +93,9 @@ class Plan(models.Model):
         """
         if (geolevel != settings.BASE_GEOLEVEL):
             base_geounit_ids = Geounit.get_base_geounits(geounit_ids, geolevel)
+        else:
+            base_geounit_ids = geounit_ids
+
         # get the geometry of all these geounits that are being added
         geounits = Geounit.objects.filter(id__in=geounit_ids).iterator()
         incremental = None
@@ -92,14 +107,25 @@ class Plan(models.Model):
 
         # incremental is the geometry that is changing
 
-        target = District.objects.filter(district_id=districtid)[0]
+        target = District.objects.get(district_id=districtid)
 
-        fixed = self.delete_geounits_prefetch(base_geounit_ids, incremental, districtid)
-        geounits = list(Geounit.objects.filter(id__in=base_geounit_ids))
-        for geounit in geounits:
-            if not target.geounits.filter(id=geounit.id):
-                target.geounits.add(geounit) 
-                fixed += 1
+        DistrictGeounitMapping.objects.filter(geounit__in=base_geounit_ids, plan=self).update(district=target)
+
+        fixed = 0
+        districts = District.objects.filter(geom__intersects=incremental)
+        for district in districts:
+            difference = district.geom.difference(incremental)
+            if difference.geom_type != 'MultiPolygon':
+                district.geom = MultiPolygon(difference)
+            else:
+                district.geom = difference
+            simple = district.geom.simplify(tolerance=100.0,preserve_topology=True)
+            if simple.geom_type != 'MultiPolygon':
+                district.simple = MultiPolygon(simple)
+            else:
+                district.simple = simple
+            district.save()
+            fixed += 1
 
         if not target.geom is None:
             union = target.geom.union(incremental)
@@ -107,55 +133,71 @@ class Plan(models.Model):
                 target.geom = MultiPolygon(union)
             else:
                 target.geom = union
+
+            simple = target.geom.simplify(tolerance=100.0,preserve_topology=True)
+            if simple.geom_type != 'MultiPolygon':
+                target.simple = MultiPolygon(simple)
+            else:
+                target.simple = simple
             
         target.save();
-        return fixed
-
-
-    def delete_geounits(self, geounit_ids, geolevel):
-        """Delete the requested geounits from given district       
-        Will return the number of districts effected by the operation
-        """
-        if (geolevel != settings.BASE_GEOLEVEL):
-            base_geounit_ids = Geounit.get_base_geounits(geounit_ids, geolevel)
-        else:
-            base_geounit_ids = geounit_ids
-
-        # get the geometry of all the geounits that are being removed
-        geounits = Geounit.objects.filter(id__in=base_geounit_ids).iterator()
-        incremental = None
-        for geounit in geounits:
-            if incremental is None:
-                incremental = geounit.geom
-            else:
-                incremental = geounit.geom.union(incremental)
-
-        return self.delete_geounits_prefetch(base_geounit_ids, incremental, None)
-
-    def delete_geounits_prefetch(self, base_geounit_ids, incremental, districtid):
-        neighbors = self.district_set.all()
-        if districtid:
-            target = District.objects.filter(district_id=districtid)[0]
-        else:
-            target = {}
-        # neighbors = District.objects.filter(plan=target.plan)
-        fixed = 0
-        for neighbor in neighbors:
-            if neighbor == target:
-                continue
-
-            geounits = neighbor.geounits.filter(id__in=base_geounit_ids).iterator()
-            changed = False
-            for geounit in geounits:
-                neighbor.geounits.remove(geounit)
-                fixed += 1
-                changed = True
-
-            if changed and not neighbor.geom is None:
-                neighbor.geom = neighbor.geom.difference(incremental)
-                neighbor.save()
+        fixed += 1
 
         return fixed
+
+
+#    def delete_geounits(self, geounit_ids, geolevel):
+#        """Delete the requested geounits from given district       
+#        Will return the number of districts effected by the operation
+#        """
+#        if (geolevel != settings.BASE_GEOLEVEL):
+#            base_geounit_qs = Geounit.get_base_geounits(geounit_ids, geolevel).iterator()
+#            base_geounit_ids = []
+#            for base_geounit_id in base_geounit_qs:
+#                base_geounit_ids.append(base_geounit_id)
+#        else:
+#            base_geounit_ids = geounit_ids
+#
+#
+#        # get the geometry of all the geounits that are being removed
+#        geounits = Geounit.objects.filter(id__in=base_geounit_ids).iterator()
+#        incremental = None
+#        for geounit in geounits:
+#            if incremental is None:
+#                incremental = geounit.geom
+#            else:
+#                incremental = geounit.geom.union(incremental)
+#
+#        return self.delete_geounits_prefetch(base_geounit_ids, incremental, None)
+#
+#    def delete_geounits_prefetch(self, base_geounit_ids, incremental, districtid):
+#        neighbors = self.district_set.all()
+#        if districtid:
+#            target = District.objects.filter(district_id=districtid)[0]
+#        else:
+#            target = {}
+#        # neighbors = District.objects.filter(plan=target.plan)
+#        fixed = 0
+#        for neighbor in neighbors:
+#            if neighbor == target:
+#                continue
+#
+#            geounits = neighbor.geounits.filter(id__in=base_geounit_ids).iterator()
+#            changed = False
+#            for geounit in geounits:
+#                neighbor.geounits.remove(geounit)
+#                fixed += 1
+#                changed = True
+#
+#            if changed and not neighbor.geom is None:
+#                difference = neighbor.geom.difference(incremental)
+#                if difference.geom_type != 'MultiPolygon':
+#                    neighbor.geom = MultiPolygon(difference)
+#                else:
+#                    neighbor.geom = difference
+#                neighbor.save()
+#
+#        return fixed
 
 class PlanForm(ModelForm):
     class Meta:
@@ -166,7 +208,6 @@ class District(models.Model):
     district_id = models.PositiveIntegerField(default=1)
     name = models.CharField(max_length=200)
     plan = models.ForeignKey(Plan)
-    geounits = models.ManyToManyField(Geounit)
     geom = models.MultiPolygonField(srid=3785, blank=True, null=True)
     simple = models.MultiPolygonField(srid=3785, blank=True, null=True)
     version = models.PositiveIntegerField(default=0)
@@ -174,6 +215,16 @@ class District(models.Model):
     
     def __unicode__(self):
         return self.name
+
+class DistrictGeounitMapping(models.Model):
+    district = models.ForeignKey(District)
+    geounit = models.ForeignKey(Geounit)
+    plan = models.ForeignKey(Plan)
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return "Plan '%s', district '%s': '%s'" % (self.plan.name,self.district.name, self.geounit.name)
+
 
 class ComputedCharacteristic(models.Model):
     subject = models.ForeignKey(Subject)
