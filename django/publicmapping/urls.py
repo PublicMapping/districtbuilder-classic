434e9c99323e9e846aef23aeea2db5be1016482c
David Zwarg <dzwarg@azavea.com>
2010-08-23 19:00:16 +0000

    Added static serving URLs accessible only when debugging.

--- django/publicmapping/urls.py
+++ django/publicmapping/urls.py
@@ -15,12 +15,20 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    (r'^site-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
     (r'^accounts/register/$', 'publicmapping.views.userregister'),
     (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'index.html'}),
     (r'^accounts/logout/$', 'publicmapping.views.userlogout'),
     (r'^districtmapping/', include('publicmapping.redistricting.urls')),
     (r'^$', 'publicmapping.views.index'),
     (r'^proxy$', 'publicmapping.views.proxy'),
-    (r'^sld/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.SLD_ROOT }),
 )
+
+# Only if this application is running in debug mode, serve up the static
+# content via django. In a production environment, these files should be 
+# served by apache.
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+        (r'^sld/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.SLD_ROOT }),
+    )
+
