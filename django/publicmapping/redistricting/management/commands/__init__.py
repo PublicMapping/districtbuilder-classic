11ce85a0c8cd104f5bcad4dc55d181ed8933ee7c
David Zwarg <dzwarg@azavea.com>
2010-08-30 17:25:44 +0000

    Added import management command.

---
+++ django/publicmapping/redistricting/management/commands/__init__.py
@@ -0,0 +1,26 @@
+#
+#   Copyright 2010 Micah Altman, Michael McDonald
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   This file is part of The Public Mapping Project
+#   http://sourceforge.net/projects/publicmapping/
+#
+#   Purpose:
+#       Define publicmapping.management.commands as a Python package.
+#
+#       This file serves the purpose of defining a Python package for
+#       this folder. It is intentionally left empty.
+#
+#   Author: David Zwarg, Andrew Jennings
+#
