810755feabeef1e4bf903ac608884ed50db65533
David Zwarg <dzwarg@azavea.com>
2010-08-31 19:56:00 +0000

    Added license headers to views and tests.

--- django/publicmapping/redistricting/views.py
+++ django/publicmapping/redistricting/views.py
@@ -1,3 +1,32 @@
+"""
+Define the views used by the redistricting app.
+
+The methods in redistricting.views define the views used to interact with
+the models in the redistricting application. Each method relates to one 
+type of output url. There are views that return GeoJSON, JSON, and HTML.
+
+This file is part of The Public Mapping Project
+http://sourceforge.net/projects/publicmapping/
+
+License:
+    Copyright 2010 Micah Altman, Michael McDonald
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+Author: 
+    David Zwarg, Andrew Jennings
+"""
+
 from django.http import *
 from django.core import serializers
 from django.core.exceptions import ValidationError
