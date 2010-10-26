9c32d47d112e4a7f792363f41a97ced73da6ecc2
David Zwarg <dzwarg@azavea.com>
2010-10-26 01:31:33 +0000

    Used ST_SIMPLIFYPRESERVETOPOLOGY, instead of ST_SIMPLIFY

--- sql/nesting_fix.sql
+++ sql/nesting_fix.sql
@@ -2,11 +2,11 @@
 
 select substring(supplemental_id, 3, 3) as county , st_union(geom) into temp_county from redistricting_geounit where geolevel_id = 3 group by county;
 
-update redistricting_geounit set geom = st_multi(st_union), simple = ST_MULTI(ST_SIMPLIFY(st_union, 10)) from temp_county where temp_county.county = supplemental_id;
+update redistricting_geounit set geom = st_multi(st_union), simple = ST_MULTI(ST_SIMPLIFYPRESERVETOPOLOGY(st_union, 10)) from temp_county where temp_county.county = supplemental_id;
 
 select substring(supplemental_id, 0, 12) || '.0' as tract , st_union(geom) into temp_tracts from redistricting_geounit where geolevel_id = 3 group by tract;
 
-update redistricting_geounit set geom = st_multi(st_union), simple = ST_MULTI(ST_SIMPLIFY(st_union, 10)) from temp_tracts where temp_tracts.tract = supplemental_id;
+update redistricting_geounit set geom = st_multi(st_union), simple = ST_MULTI(ST_SIMPLIFYPRESERVETOPOLOGY(st_union, 10)) from temp_tracts where temp_tracts.tract = supplemental_id;
 
 drop table temp_county;
 drop table temp_tracts;
