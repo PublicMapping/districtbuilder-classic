313805dd2b034a85797f134ba5032b75eec2f0d9
David Zwarg <dzwarg@azavea.com>
2011-03-21 18:58:59 +0000

    Updated autodoc.sh to use the proper appname.

--- docs/autodoc.sh
+++ docs/autodoc.sh
@@ -5,7 +5,7 @@ epydoc --config=epydoc.config
 
 # Generate the DOT code for graphviz. This will prompt for a password
 echo "Password for database 'publicmapping':"
-postgresql_autodoc -d publicmapping -h localhost -p 5432 -u publicmapping --password -s publicmapping -m "publicmapping_" -t dot
+postgresql_autodoc -d publicmapping -h localhost -p 5432 -u publicmapping --password -s publicmapping -m "redistricting_" -t dot
 
 # Generate a graphic from the dot file.
 dot -Tpng publicmapping.dot -o html/schema.png
