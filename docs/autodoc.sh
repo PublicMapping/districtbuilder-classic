7d97567c3909186a5b4bcb2e742cc0838f7befa2
David Zwarg <dzwarg@azavea.com>
2011-03-21 15:08:21 +0000

    Updating autogenerated docs.

--- docs/autodoc.sh
+++ docs/autodoc.sh
@@ -4,10 +4,11 @@
 epydoc --config=epydoc.config
 
 # Generate the DOT code for graphviz. This will prompt for a password
+echo "Password for database 'publicmapping':"
 postgresql_autodoc -d publicmapping -h localhost -p 5432 -u publicmapping --password -s publicmapping -m "publicmapping_" -t dot
 
 # Generate a graphic from the dot file.
-dot -Tpng html/publicmapping.dot -o html/schema.png
+dot -Tpng publicmapping.dot -o html/schema.png
 
-rm html/publicmapping.dot
+rm publicmapping.dot
 cp html/schema.png schema.png
