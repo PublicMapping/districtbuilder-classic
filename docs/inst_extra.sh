9410b60669eb836d6c6120526621acb40f14bc00
Micah Altman <micah_altman@harvard.edu>
2011-04-19 18:50:18 +0000

    updating for current version of ubuntu, adding ogrinfo, usage reporting

--- docs/inst_extra.sh
+++ docs/inst_extra.sh
@@ -11,17 +11,53 @@
 #
 # Install mail support
 # 
+echo "Installing gdal"
+apt-get -y install gdal-bin
+
+#
+# Install mail support
+# 
 echo "Installing mail support"
 apt-get -y install mailutils
+apt-get -y install sharutils
 apt-get -y install sendmail-bin
+
+SMPATCH='*** /etc/mail/sendmail.mc.good       2010-12-15 22:01:17.773507002 +0000
+--- /etc/mail/sendmail.mc 2011-04-19 17:46:41.635571035 +0000
+***************
+*** 107,109 ****
+--- 107,112 ----
+  MASQUERADE_AS(`publicmapping.org'\'')dnl
+  FEATURE(`allmasquerade'\'')dnl
+  FEATURE(`masquerade_envelope'\'')dnl
++
++ # max file size DOS prevention
++ define(`confMAX_MESSAGE_SIZE'\'',`524288'\'')dnl
+'
+
+echo "$SMPATCH" | patch -bN /etc/mail/sendmail.mc
 
 #
 # Install report directory cleanup
 # 
-echo "Installing mail support"
+echo "Installing directory cleanup"
 apt-get -y install tmpreaper
 echo "5 0 * * *      /usr/sbin/tmpreaper 2d /projects/publicmapping/local/reports/ /tmp/ > /dev/null 2>&1" > /tmp/wwwcron
 crontab -u www-data /tmp/wwwcron
+
+
+#
+# Install usage reporting
+#
+
+echo "Installing cron job to send usage reports to publicmapping.org"
+USAGEREP='#!/bin/sh
+
+# send usage reports
+(cut -d" " -f 1,4-7 /var/log/apache2/publicmapping-access.log | grep /district | cut -d . -f3-  | gzip -c | uuencode - | mail -s "logs from `hostname -f`" support@publicmapping.org) 1>/dev/null 2>/dev/null
+'
+echo "$USAGEREP"> /etc/cron.daily/publicmapping
+chmod a+x,a+r /etc/cron.daily/publicmapping
 
 
 # Configure Automatic Security Updates 
@@ -30,19 +66,13 @@ crontab -u www-data /tmp/wwwcron
 #
 
 echo "Configuring auto-update"
-PDCONTENTS='APT::Periodic::Enable "1"; 
-APT::Periodic::Update-Package-Lists "1"; 
-APT::Periodic::Download-Upgradeable-Packages "1"; 
-APT::Periodic::AutocleanInterval "5"; 
-APT::Periodic::Unattended-Upgrade "1";
-APT::Periodic::RandomSleep "1800"; 
-'
-/bin/echo "$PDCONTENTS"  > /etc/apt/apt.conf.d/10periodic
 
-PDPATCH='*** /etc/apt/apt.conf.d/50unattended-upgrades.orig	2010-04-29 14:46:10.000000000 +0000
---- /tmp/50unattended-upgrades.orig	2010-10-24 17:53:48.000000000 +0000
+apt-get -y install unattended-upgrades
+PDPATCH='
+*** 50unattended-upgrades.orig  2010-08-26 16:48:50.000000000 +0000
+--- 50unattended-upgrades.good  2011-04-19 18:10:42.345571039 +0000
 ***************
-*** 16,22 ****
+*** 18,24 ****
   // If empty or unset then no email is sent, make sure that you
   // have a working mail setup on your system. The package 'mailx'
   // must be installed or anything that provides /usr/bin/mail.
@@ -50,16 +80,16 @@ PDPATCH='*** /etc/apt/apt.conf.d/50unattended-upgrades.orig	2010-04-29 14:46:10.
   
   // Do automatic removal of new unused dependencies after the upgrade
   // (equivalent to apt-get autoremove)
---- 16,22 ----
+--- 18,24 ----
   // If empty or unset then no email is sent, make sure that you
-  // have a working mail setup on your system. The package 'mailx'
+  // have a working mail setup on your system. The package '\''mailx'\''
   // must be installed or anything that provides /usr/bin/mail.
 ! Unattended-Upgrade::Mail "root@localhost";
   
   // Do automatic removal of new unused dependencies after the upgrade
   // (equivalent to apt-get autoremove)
 ***************
-*** 24,32 ****
+*** 26,34 ****
   
   // Automatically reboot *WITHOUT CONFIRMATION* if a 
   // the file /var/run/reboot-required is found after the upgrade 
@@ -70,16 +100,17 @@ PDPATCH='*** /etc/apt/apt.conf.d/50unattended-upgrades.orig	2010-04-29 14:46:10.
   // speed to 70kb/sec
 ! //Acquire::http::Dl-Limit "70";
 \ No newline at end of file
---- 24,32 ----
+--- 26,34 ----
   
   // Automatically reboot *WITHOUT CONFIRMATION* if a 
   // the file /var/run/reboot-required is found after the upgrade 
-! Unattended-Upgrade::Automatic-Reboot "false";
+! Unattended-Upgrade::Automatic-Reboot "true";
   
   
   // Use apt bandwidth limit feature, this example limits the download
   // speed to 70kb/sec
-! //Acquire::http::Dl-Limit "70";'
+! //Acquire::http::Dl-Limit "70";
+'
 echo "$PDPATCH" | patch -bN /etc/apt/apt.conf.d/50unattended-upgrades
 
 echo "Installing security patches -- this may reboot at end"
