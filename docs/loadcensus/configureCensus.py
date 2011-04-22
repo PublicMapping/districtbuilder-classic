6fe2491c2b84530fdd7bfabd3f55d90dfef3c438
David Zwarg <dzwarg@azavea.com>
2011-04-22 14:55:36 +0000

    Brought back whitespace fix to census loader.

--- docs/loadcensus/configureCensus.py
+++ docs/loadcensus/configureCensus.py
@@ -160,9 +160,7 @@ class Empty_Template(ListTemplate):
 class SldList_Template(DictionaryTemplate):
     _template = """
 <?xml version="1.0" encoding="ISO-8859-1"?>
-<StyledLayerDescriptor version="1.0.0" xmlns="http://www.opengis.net/sld" xmlns:ogc="http://
-www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-ins
-tance"  xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd">
+<StyledLayerDescriptor version="1.0.0" xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd">
   <NamedLayer>
     <Name>%(layername)s</Name>
     <UserStyle>
