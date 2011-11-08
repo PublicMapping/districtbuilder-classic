c0f94359feebd31c804d5878ae56a53377fdb945
David Zwarg <dzwarg@azavea.com>
2011-11-08 12:57:02 -0500

    Added SQL for subject upload data.

---
+++ sql/024_add_subject_upload.sql
@@ -0,0 +1,20 @@
+--
+-- Add uploaded subject quarantine table and metadata table
+--
+CREATE TABLE "redistricting_subjectupload" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "filename" varchar(256) NOT NULL,
+    "subject_name" varchar(50) NOT NULL,
+    "status" varchar(2) NOT NULL,
+    "task_id" varchar(36) NOT NULL
+)
+;
+CREATE TABLE "redistricting_subjectquarantine" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "upload_id" integer NOT NULL REFERENCES "redistricting_subjectupload" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "portable_id" varchar(50) NOT NULL,
+    "number" numeric(12, 4) NOT NULL
+)
+;
+CREATE INDEX "redistricting_subjectquarantine_upload_id" ON "redistricting_subjectquarantine" ("upload_id");
+
