c62e1b410035a78491df52f0e2f465a365d9227f
Kenny Shepard <kshepard@gmail.com>
2011-11-16 11:26:38 -0500

    Add language message files, their compiled counterparts, and a management command

---
+++ django/publicmapping/redistricting/management/commands/makelanguagefiles.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from django.core.management.base import BaseCommand
+from redistricting.utils import *
+
+class Command(BaseCommand):
+    """
+    This command prints creates and compiles language message files
+    """
+    args = None
+    help = 'Create and compile language message files'
+
+    def handle(self, *args, **options):
+        """
+        Create and compile language message files
+        """
+        # Make messages for each language defined in settings
+        for language in settings.LANGUAGES:
+            management.call_command('makemessages', locale=language[0], interactive=False)
+
+        # Compile all message files
+        management.call_command('compilemessages', interactive=False)
