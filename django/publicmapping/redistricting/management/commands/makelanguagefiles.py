15c58fb05a9bfb06b87d8d00a1b26d50ee68c1f7
Kenny Shepard <kshepard@gmail.com>
2011-11-17 14:04:43 -0500

    Add creation of js message files to management command

--- django/publicmapping/redistricting/management/commands/makelanguagefiles.py
+++ django/publicmapping/redistricting/management/commands/makelanguagefiles.py
@@ -15,7 +15,11 @@ class Command(BaseCommand):
         """
         # Make messages for each language defined in settings
         for language in settings.LANGUAGES:
+            # For django templates
             management.call_command('makemessages', locale=language[0], interactive=False)
+
+            # For javascript files
+            management.call_command('makemessages', domain='djangojs', locale=language[0], interactive=False)
 
         # Compile all message files
         management.call_command('compilemessages', interactive=False)
