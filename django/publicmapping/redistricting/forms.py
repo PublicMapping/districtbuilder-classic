68e443e739cc515503aa67cdd1e705ad07f86b4f
David Zwarg <dzwarg@azavea.com>
2011-11-17 10:42:32 -0500

    Added copyright and license notices to new files added for subject upload.

--- django/publicmapping/redistricting/forms.py
+++ django/publicmapping/redistricting/forms.py
@@ -1,3 +1,32 @@
+"""
+Define the forms used by the redistricting app.
+
+The forms in redistricting.forms define the UI forms used in the 
+application. Each class relates to a form used to capture user input
+in the application.
+
+This file is part of The Public Mapping Project
+https://github.com/PublicMapping/
+
+License:
+    Copyright 2010-2011 Micah Altman, Michael McDonald
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+Author: 
+    Andrew Jennings, David Zwarg, Kenny Shepard
+"""
+
 from models import *
 from django import forms
 from django.core.files.uploadedfile import UploadedFile
