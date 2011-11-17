7448b5e266b2a8c3ebb2cb7a4fbe3eea44a238cb
David Zwarg <dzwarg@azavea.com>
2011-11-17 17:06:27 -0500

    Refactored some XML logic into config.py, and added unit tests.

--- django/publicmapping/redistricting/tests.py
+++ django/publicmapping/redistricting/tests.py
@@ -40,9 +40,11 @@ from models import *
 from utils import *
 from calculators import *
 from reportcalculators import *
+from config import *
 from django.conf import settings
 from datetime import datetime
 from tagging.models import Tag, TaggedItem
+import tempfile
 
 class BaseTestCase(TestCase):
     """
@@ -4299,3 +4301,123 @@ class RegionConfigTest(BaseTestCase):
 
     def tearDown(self):
         self.region_tree = None
+
+class ConfigTestCase(TestCase):
+    good_data_filename = '../../docs/config.dist.xml'
+    good_schema_filename = '../../docs/config.xsd'
+    bad_data_filename = '/tmp/bad_data.xsd'
+    bad_schema_filename = '/tmp/bad_schema.xsd'
+
+    def make_simple_schema(self):
+        test_schema = tempfile.NamedTemporaryFile(delete=False)
+        test_schema.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        test_schema.write('<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">\n')
+        test_schema.write('<xs:element name="DistrictBuilder"/>\n')
+        test_schema.write('</xs:schema>')
+        test_schema.close()
+
+        return test_schema
+
+    """
+    Test the Config classes.
+    """
+    def test_constructor_missing_schema(self):
+        try:
+            x = StoredConfig(self.bad_data_filename, schema=self.bad_schema_filename)
+            self.fail('Expected failure when passing nonexistant filenames to constructor.')
+        except:
+            pass
+
+    def test_construct_missing_data(self):
+        try:
+            x = StoredConfig(self.bad_data_filename, schema=self.good_schema_filename)
+            self.fail('Expected failure when passing nonexistant filenames to constructor.')
+        except:
+            pass
+
+    def test_construct_okay(self):
+        x = StoredConfig(self.good_data_filename, schema=self.good_schema_filename)
+
+        self.assertEqual(x.datafile, self.good_data_filename, 'Configuration data is not correct.')
+        self.assertEqual(x.schemafile, self.good_schema_filename, 'Configuration schema is not correct.')
+
+    def test_validation_schema_xml_parser(self):
+        test_schema = tempfile.NamedTemporaryFile(delete=False)
+        test_schema.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        test_schema.write('<DistrictBuilder><junk></DistrictBuilder>\n')
+        test_schema.close()
+
+        x = StoredConfig(self.good_data_filename, schema=test_schema.name)
+        (is_valid, msgs,) = x.validate()
+
+        os.remove(test_schema.name)
+
+        self.assertFalse(is_valid, 'Configuration schema was not valid.')
+        self.assertEqual(3, len(msgs), 'The number of XML parsing errors is incorrect.')
+        self.assertEqual(msgs[0], 'The schema XML file could not be parsed.', 'Error message does not indicate proper failure for schema parsing failure.')
+
+    def test_validation_schema_xml_wellformed(self):
+        test_schema = tempfile.NamedTemporaryFile(delete=False)
+        test_schema.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        test_schema.write('<DistrictBuilder><junk/></DistrictBuilder>\n')
+        test_schema.close()
+
+        x = StoredConfig(self.good_data_filename, schema=test_schema.name)
+        (is_valid, msgs,) = x.validate()
+
+        os.remove(test_schema.name)
+
+        self.assertFalse(is_valid, 'Configuration schema was not valid.')
+        self.assertEqual(len(msgs), 2, 'The number of XML parsing errors is incorrect.')
+        self.assertEqual(msgs[0], 'The schema XML file was parsed, but it does not appear to be a valid XML Schema document.', 'Error message does not indicate proper failure for schema parsing failure.')
+
+    def test_validation_schema_xml(self):
+        test_schema = self.make_simple_schema()
+
+        x = StoredConfig(self.good_data_filename, schema=test_schema.name)
+        (is_valid, msgs,) = x.validate()
+
+        os.remove(test_schema.name)
+
+        self.assertTrue(is_valid, 'Configuration schema should be valid.')
+        self.assertEqual(len(msgs), 0, 'The number of XML parsing errors is incorrect.')
+
+    def test_validation_data_xml_parser(self):
+        test_schema = self.make_simple_schema()
+
+        test_data = tempfile.NamedTemporaryFile(delete=False)
+        test_data.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        test_data.write('<DistrictBuilder><junk></DistrictBuilder>\n')
+        test_data.close()
+
+        x = StoredConfig(test_data.name, schema=test_schema.name)
+        (is_valid, msgs,) = x.validate()
+
+        os.remove(test_data.name)
+        os.remove(test_schema.name)
+
+        self.assertFalse(is_valid, 'Configuration data was not valid.')
+        self.assertEqual(3, len(msgs), 'The number of XML parsing errors is incorrect.')
+        self.assertEqual(msgs[0], 'The data XML file could not be parsed.', 'Error message does not indicate proper failure for data parsing failure.')
+
+    def test_validation_data_xml_wellformed(self):
+        test_schema = self.make_simple_schema()
+        test_data = tempfile.NamedTemporaryFile(delete=False)
+        test_data.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        test_data.write('<DistrictBuilder><junk/></DistrictBuilder>\n')
+        test_data.close()
+
+        x = StoredConfig(test_data.name, schema=test_schema.name)
+        (is_valid, msgs,) = x.validate()
+
+        os.remove(test_data.name)
+        os.remove(test_schema.name)
+
+        self.assertTrue(is_valid, 'Configuration data was not valid.')
+
+    def test_validation(self):
+        x = StoredConfig(self.good_data_filename, schema=self.good_schema_filename)
+
+        (is_valid, msgs,) = x.validate()
+
+        self.assertTrue(is_valid, 'Configuration schema and data should be valid and should validate successfully.')
