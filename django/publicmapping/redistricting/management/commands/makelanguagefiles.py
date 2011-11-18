044d4de8cfb36d015d97653ec59c8ffa27334b7e
Kenny Shepard <kshepard@gmail.com>
2011-11-18 16:29:11 -0500

    Add options to makelanguagefiles management command

--- django/publicmapping/redistricting/management/commands/makelanguagefiles.py
+++ django/publicmapping/redistricting/management/commands/makelanguagefiles.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from django.core.management.base import BaseCommand
 from redistricting.utils import *
+from optparse import make_option
 
 class Command(BaseCommand):
     """
@@ -8,18 +9,34 @@ class Command(BaseCommand):
     """
     args = None
     help = 'Create and compile language message files'
+    option_list = BaseCommand.option_list + (
+        make_option('-t', '--templates', dest="templates", action="store_true", help="Create template message files.", default=False),
+        make_option('-j', '--javascript', dest="javascript", action="store_true", help="Create javascript message files.", default=False),
+        make_option('-c', '--compile', dest="compile", action="store_true", help="Compile message files.", default=False),
+        make_option('-l', '--locale', dest='locale', default=None, action='store', help='Specify a locale.'),
+    )
 
     def handle(self, *args, **options):
         """
         Create and compile language message files
         """
-        # Make messages for each language defined in settings
-        for language in settings.LANGUAGES:
-            # For django templates
-            management.call_command('makemessages', locale=language[0], interactive=False)
+        # Execute every action if either all or none of the options are specified
+        everything = options.get("templates") == options.get("javascript") == options.get("compile")
 
-            # For javascript files
-            management.call_command('makemessages', domain='djangojs', locale=language[0], interactive=False)
+        # Use the specified locale, or if none are provided, use all defined in settings
+        locale = options.get("locale")
+        locales = [locale] if locale else [l[0] for l in settings.LANGUAGES]
+        
+        # Make messages for each available locale
+        for locale in locales:
+            # Make messages for templates (.html, .txt, .email)
+            if everything or options.get("templates"):
+                management.call_command('makemessages', locale=locale, extension="html,txt,email", interactive=False)    
+        
+            # Make messages for javascript
+            if everything or options.get("javascript"):
+                management.call_command('makemessages', locale=locale, domain='djangojs', interactive=False)
 
-        # Compile all message files
-        management.call_command('compilemessages', interactive=False)
+            # Compile message file
+            if everything or options.get("compile"):
+                management.call_command('compilemessages', locale=locale, interactive=False)
