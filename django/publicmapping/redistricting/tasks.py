6b0d1c13ac2998133e66cd6c167f0a015bf4b565
David Zwarg <dzwarg@azavea.com>
2011-12-21 17:19:02 -0500

    Converted to logging with 'logging', and not stderr.

--- django/publicmapping/redistricting/tasks.py
+++ django/publicmapping/redistricting/tasks.py
@@ -177,7 +177,7 @@ class DistrictIndexFile():
                         email_template = loader.get_template('admin.email')
                         mail_admins(admin_subject, email_template.render(context))
                     else:
-                        sys.stderr.write('District Index .zip file contains too many files.\n')
+                        logger.warn('District Index .zip file contains too many files.')
                     return
 
                 item = archive.namelist()[0]
@@ -197,7 +197,7 @@ class DistrictIndexFile():
                         email_template = loader.get_template('admin.email')
                         mail_admins(admin_subject, email_template.render(context))
                     else:
-                        sys.stderr.write('District Index .zip file does not contain a .csv file.\n')
+                        logger.warn('District Index .zip file does not contain a .csv file.\n')
 
                     return
 
@@ -220,7 +220,7 @@ class DistrictIndexFile():
                     try:
                         os.unlink(filename)
                     except:
-                        sys.stderr.write('Could not unlink file: "%s":\n' % filename)
+                        logger.info('Could not unlink file: "%s"', filename)
 
                 indexFile = dest.name
 
@@ -233,8 +233,9 @@ class DistrictIndexFile():
                     # report error to admin
                     email_template = loader.get_template('admin.email')
                     mail_admins(admin_subject, email_template.render(context))
-                else:
-                    sys.stderr.write('The .zip file could not be imported:\n%s\n' % traceback.format_exc())
+                else Exception, ex2:
+                    logger.info('The .zip file could not be imported')
+                    logger.debug('Reason:', ex2)
 
                 if purge:
                     # Some problem opening the zip file, bail now
@@ -245,7 +246,7 @@ class DistrictIndexFile():
             indexFile = filename
 
             if not os.exists(indexFile):
-                sys.stderr.write('The .csv file could not be found, plan "%s" was not created.' % name)
+                logger.warn('The .csv file could not be found, plan "%s" was not created.', name)
                 return
 
         try:
@@ -262,8 +263,9 @@ class DistrictIndexFile():
                 send_mail(error_subject, template.render(context), settings.EMAIL_HOST_USER, [email], fail_silently=False)
                 template = loader.get_template('admin.email')
                 mail_admins(error_subject, template.render(context))
-            else:
-                sys.stderr.write('The plan "%s" could not be created:\n%s\n' % (name, traceback.format_exc()))
+            else Exception, ex:
+                logger.warn('The plan "%s" could not be created', name)
+                logger.debug('Reason:', ex)
             return
                 
         # initialize the dicts we'll use to store the portable_ids,
@@ -303,8 +305,9 @@ class DistrictIndexFile():
                         'message': 'Did not import row:\n  "%s, %s"\n' % (row['code'], row['district']),
                         'traceback': traceback.format_exc()
                     })
-                else:
-                    sys.stderr.write("Did not import row:\n  '%s'\nReason:\n  %s\n" % (row, traceback.format_exc()))
+                else Exception, ex:
+                    logger.debug("Did not import row: '%s'", row)
+                    logger.debug(ex)
         csv_file.close()
 
         if purge:
@@ -351,14 +354,15 @@ class DistrictIndexFile():
                             if strtag:
                                 Tag.objects.add_tag(new_district, 'type=%s' % strtag)
                 
-            except Exception as ex:
+            except Exception, ex:
                 if email:
                     context['errors'].append({
                         'message': 'Unable to create district %s.' % district_id,
                         'traceback': traceback.format_exc()
                     })
                 else:
-                    sys.stderr.write('Unable to create district %s.\nReason:\n  %s\n' % (district_id, traceback.format_exc()))
+                    logger.warn('Unable to create district %s.', district_id)
+                    logger.debug('Reason:', ex)
                 continue
         
             # For each district, create the ComputedCharacteristics
@@ -379,23 +383,23 @@ class DistrictIndexFile():
                                 district = new_district).number
                             percentage = value / denominator_value
 
-                        # sys.stderr.write('Aggregating value for %s: %s' % (subject, cc_value))
                         cc = ComputedCharacteristic(subject = subject, 
                             number = value, 
                             percentage = percentage,
                             district = new_district)
                         cc.save()
                     else:
-                        sys.stderr.write('Unable to create ComputedCharacteristic for Subject: %s. Skipping subject\n' % subject.name)
+                        logger.debug('Unable to create ComputedCharacteristic for Subject: %s. Skipping subject.', subject.name)
                         continue
-                except Exception as ex:
+                except Exception, ex:
                     if email:
                         context['errors'].append({
                             'message': 'Unable to create ComputedCharacteristic for district %s, subject %s' % (district_id, subject.name),
                             'traceback': None
                         })
                     else:
-                        sys.stderr.write('Unable to create ComputedCharacteristic for district %s, subject %s\nReason:\n  %s\n' % (district_id, subject.name, traceback.format_exc()))
+                        logger.debug('Unable to create ComputedCharacteristic for district %s, subject %s', district_id, subject.name)
+                        logger.debug('Reason:', ex)
 
         # Now that all of our other districts exist, create an unassigned district
         plan.create_unassigned = True
@@ -467,8 +471,9 @@ class DistrictIndexFile():
                 zipwriter.close()
                 archive.close()
                 os.rename(archive.name, DistrictFile.get_file_name(plan) + '.zip')
-            except Exception as ex:
-                sys.stderr.write('The plan "%s" could not be serialized to a district index file:\n%s\n' % (plan.name, traceback.format_exc()))
+            except Exception, ex:
+                logger.warn('The plan "%s" could not be serialized to a district index file', plan.name)
+                logger.debug('Reason:', ex)
                 os.unlink(archive.name)
             # delete the temporary csv file
             finally:
@@ -906,8 +911,9 @@ class DistrictShapeFile():
                 zipwriter.close()
                 archive.close()
                 os.rename(archive.name, DistrictFile.get_file_name(plan,True) + '.zip')
-            except Exception as ex:
-                sys.stderr.write('The plan "%s" could not be saved to a shape file:\n%s\n' % (plan.name, traceback.format_exc()))
+            except Exception, ex:
+                logger.warn('The plan "%s" could not be saved to a shape file', plan.name)
+                logger.debug('Reason:', ex)
                 os.unlink(archive.name)
             # delete the temporary csv file
             finally:
@@ -940,19 +946,17 @@ class PlanReport:
         Create the data structures required for a BARD report, and call
         the django reporting apache process to create the report.
         """
-        if settings.DEBUG:
-            sys.stderr.write('Starting task to create a report.\n')
+        logger.debug('Starting task to create a report.')
         try:
             plan = Plan.objects.get(pk=planid)
         except:
-            sys.stderr.write("Couldn't retrieve plan information.\n")
+            logger.warn("Couldn't retrieve plan information for plan %d.", planid)
             return 
 
         tempdir = settings.WEB_TEMP
         filename = '%s_p%d_v%d_%s' % (plan.owner.username, plan.id, plan.version, stamp)
 
-        if settings.DEBUG:
-            sys.stderr.write('Getting base geounits.\n')
+        logger.debug('Getting base geounits.')
 
         # Get the district mapping and order by geounit id
         mapping = plan.get_base_geounits()
@@ -965,8 +969,7 @@ class PlanReport:
         min_id = int(max_and_min['id__min'])
         max_id = int(max_and_min['id__max'])
 
-        if settings.DEBUG:
-            sys.stderr.write('Getting district mapping.\n')
+        logger.debug('Getting district mapping.')
 
         # Iterate through the query results to create the district_id list
         # This ordering depends on the geounits in the shapefile matching the 
@@ -986,8 +989,7 @@ class PlanReport:
                 district_id = 'NA'
             sorted_district_list.append(str(district_id))
 
-        if settings.DEBUG:
-            sys.stderr.write('Getting POST variables and settings.')
+        logger.debug('Getting POST variables and settings.')
         
         info = plan.get_district_info()
         names = map(lambda i:i[0], info)
@@ -996,8 +998,7 @@ class PlanReport:
         # needs to be a str because of join() below
         magnitude = map(lambda i:str(i[1]), info)
 
-        if settings.DEBUG:
-            sys.stderr.write('Firing web worker task.')
+        logger.debug('Firing web worker task.')
 
         dispatcher = HttpDispatchTask()
 
@@ -1110,12 +1111,11 @@ class CalculatorReport:
         """
         Create the report.
         """
-        if settings.DEBUG:
-            sys.stderr.write('Starting task to create a report.\n')
+        logger.debug('Starting task to create a report.')
         try:
             plan = Plan.objects.get(pk=planid)
         except:
-            sys.stderr.write("Couldn't retrieve plan information.\n")
+            logger.debug("Couldn't retrieve plan information for plan %d", planid)
             return 
 
         function_ids = map(lambda s: int(s), request['functionIds'].split(','))
@@ -1125,8 +1125,8 @@ class CalculatorReport:
             display = ScoreDisplay.objects.filter(title='%s Reports' % plan.legislative_body.name)[0]
             html = display.render(plan, request, function_ids=function_ids)
         except Exception as ex:
-            err = 'Error creating calculator report:\n%s\n' % traceback.format_exc()
-            sys.stderr.write(err)
+            logger.warn('Error creating calculator report')
+            logger.debug('Reason:', ex)
             html = err
 
         # Add to report container template
