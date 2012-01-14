911a48775c690d090a2ad70ab28799c2983d6df8
David Zwarg <dzwarg@azavea.com>
2012-01-13 19:09:52 -0500

    Added XML IDs to all items with labels. Added SQL to facilitate upgrade from older versions.

---
+++ sql/026_add_i18n_support.sql
@@ -0,0 +1,6 @@
+--
+-- Add support for the i18n libraries and methods.
+--
+ALTER TABLE publicmapping.redistricting_scoredisplay ADD COLUMN "name" character varying(50) NOT NULL DEFAULT '';
+ALTER TABLE publicmapping.redistricting_scorepanel ADD COLUMN "name" character varying(50) NOT NULL DEFAULT '';
+
