f428998387216db62b498eaae9ecb887371fda04
David Zwarg <dzwarg@azavea.com>
2012-01-13 19:16:24 -0500

    Amended sql upgrade script.

--- sql/026_add_i18n_support.sql
+++ sql/026_add_i18n_support.sql
@@ -3,4 +3,5 @@
 --
 ALTER TABLE publicmapping.redistricting_scoredisplay ADD COLUMN "name" character varying(50) NOT NULL DEFAULT '';
 ALTER TABLE publicmapping.redistricting_scorepanel ADD COLUMN "name" character varying(50) NOT NULL DEFAULT '';
+ALTER TABLE publicmapping.redistricting_validationcriteria ADD COLUMN title character varying(50) NOT NULL DEFAULT '';
 
