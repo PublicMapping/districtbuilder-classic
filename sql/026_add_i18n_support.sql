3b4105b053c0af8663257b77a47bed2e9870ab2d
David Zwarg <dzwarg@azavea.com>
2012-01-13 19:32:45 -0500

    Rename contents of 'name' to 'title' for ValidationCriteria.

--- sql/026_add_i18n_support.sql
+++ sql/026_add_i18n_support.sql
@@ -4,4 +4,6 @@
 ALTER TABLE publicmapping.redistricting_scoredisplay ADD COLUMN "name" character varying(50) NOT NULL DEFAULT '';
 ALTER TABLE publicmapping.redistricting_scorepanel ADD COLUMN "name" character varying(50) NOT NULL DEFAULT '';
 ALTER TABLE publicmapping.redistricting_validationcriteria ADD COLUMN title character varying(50) NOT NULL DEFAULT '';
+UPDATE publicmapping.redistricting_validationcriteria SET title = "name";
+UPDATE publicmapping.redistricting_validationcriteria SET "name" = '';
 
