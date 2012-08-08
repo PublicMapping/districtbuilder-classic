51cbe310346bdcd682f6823155fb43c95869193e
Andrew Jennings <ajennings@azavea.com>
2012-08-08 15:04:56 -0400

    Update map info tool
    Change info tool behavior to remove "Demographic" items when empty, show a
    dialog instead.  Fix tool to delete old stats. Closes #310.

--- django/publicmapping/redistricting/static/js/mapping.js
+++ django/publicmapping/redistricting/static/js/mapping.js
@@ -116,9 +116,6 @@ function createToolTipHeader() {
 
 function createMapTipDiv() {
     var tipDiv = createToolTipHeader();
-    for (i = 1; i < 4; i++) {
-        tipDiv.append($('<div />').text(gettext("Demographic ") + i + ":"));
-    }
     tipDiv.addClass('maptip');
     return tipDiv[0];
 }
@@ -1738,6 +1735,11 @@ function mapinit(srs,maxExtent) {
             }
         }
 
+        tipdiv.style.display = 'none';
+
+        // Clear out the map tip div
+        $(tipdiv).find('.demographic').remove();
+ 
         // sort the characteristics alphabetically by label
         ctics = $(ctics).sort(function(a, b) { return a.lbl > b.lbl; });
 
@@ -1751,38 +1753,48 @@ function mapinit(srs,maxExtent) {
         var centroid = tipFeature.geometry.getCentroid();
         var lonlat = new OpenLayers.LonLat( centroid.x, centroid.y );
         var pixel = olmap.getPixelFromLonLat(lonlat);
-        tipdiv.style.display = 'block';
-        tipdiv.childNodes[0].childNodes[0].nodeValue = place;
+        $(tipdiv).find('h1').text(place);
         var select = $('#districtby')[0];
         var value = parseInt(tipFeature.attributes.number, 10);
 
-        var node = 2;
-        $(ctics).each(function(i, obj) {
-            try {
-                $(tipdiv.childNodes[node]).html(obj.lbl + ': ' + Math.round(obj.val).toLocaleString());
-                node ++;
-            } catch (exception) {
-                // too many characteristics
+        if (ctics.length > 0) {
+            $(ctics).each(function(i, obj) {
+                try {
+                    var demographic = $('<div class="demographic"/>').html(obj.lbl + ': ' + Math.round(obj.val).toLocaleString());
+                    $(tipdiv).append(demographic);
+                } catch (exception) {
+                    // too many characteristics
+                }
+            });
+
+            var halfWidth = tipdiv.clientWidth/2;
+            var halfHeight = tipdiv.clientHeight/2;
+            if (pixel.x < halfWidth) { 
+                pixel.x = halfWidth;
             }
-        });
+            else if (pixel.x > olmap.div.clientWidth - halfWidth) {
+                pixel.x = olmap.div.clientWidth - halfWidth;
+            }
+            if (pixel.y < halfHeight) {
+                pixel.y = halfHeight;
+            }
+            else if (pixel.y > (olmap.div.clientHeight-29) - halfHeight) {
+                pixel.y = (olmap.div.clientHeight-29) - halfHeight;
+            }
 
-        var halfWidth = tipdiv.clientWidth/2;
-        var halfHeight = tipdiv.clientHeight/2;
-        if (pixel.x < halfWidth) { 
-            pixel.x = halfWidth;
+            tipdiv.style.left = (pixel.x - halfWidth) + 'px';
+            tipdiv.style.top = (pixel.y - halfHeight) + 'px';
+            tipdiv.style.display = 'block';
+        } else {
+            $("<div>Couldn't retrieve demographic info for that geounit. " +
+                    "Please select another.</div>").dialog({
+                title: 'No demographics available',
+                resizable: false,
+                modal: true
+            });
         }
-        else if (pixel.x > olmap.div.clientWidth - halfWidth) {
-            pixel.x = olmap.div.clientWidth - halfWidth;
-        }
-        if (pixel.y < halfHeight) {
-            pixel.y = halfHeight;
-        }
-        else if (pixel.y > (olmap.div.clientHeight-29) - halfHeight) {
-            pixel.y = (olmap.div.clientHeight-29) - halfHeight;
-        }
+                
 
-        tipdiv.style.left = (pixel.x - halfWidth) + 'px';
-        tipdiv.style.top = (pixel.y - halfHeight) + 'px';
         if (tipdiv.pending) {
             clearTimeout(tipdiv.timeout);
             tipdiv.pending = false;
