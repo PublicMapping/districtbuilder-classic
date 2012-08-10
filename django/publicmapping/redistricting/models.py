4c5c1e7b36d6c79731f0d028dd8f6dda43f10e50
Andrew Jennings <ajennings@azavea.com>
2012-08-10 16:06:20 -0400

    Add more exception handling to configure_views
    The add/modify subject tasks seem to get stuck in a bad DB transaction state.  Wrap them up so that a bad commit doesn't exit the task without rolling back the transaction.

--- django/publicmapping/redistricting/models.py
+++ django/publicmapping/redistricting/models.py
@@ -52,6 +52,7 @@ from copy import copy
 from decimal import *
 from operator import attrgetter
 from rosetta import polib
+from traceback import format_exc
 import os, sys, cPickle, types, tagging, re, logging
 
 logger = logging.getLogger(__name__)
@@ -3913,20 +3914,37 @@ def configure_views():
         lbset = ','.join(map( lambda x:str(x.legislative_body_id), geolevel.legislativelevel_set.all()))
         sql = "CREATE OR REPLACE VIEW simple_district_%s AS SELECT rd.id, rd.district_id, rd.plan_id, st_geometryn(rd.simple, %d) AS geom, rp.legislative_body_id FROM publicmapping.redistricting_district as rd JOIN publicmapping.redistricting_plan as rp ON rd.plan_id = rp.id WHERE rp.legislative_body_id IN (%s);" % (geolevel.name, geolevel.id, lbset)
         cursor.execute(sql)
-        transaction.commit()
+        try:
+            transaction.commit()
+        except:
+            transaction.rollback()
+            logger.error('Failed to create simple_district_%s view',
+                geolevel.name)
+            logger.error(format_exc())
 
         logger.debug('Created simple_district_%s view ...', geolevel.name)
 
         sql = "CREATE OR REPLACE VIEW simple_%s AS SELECT rg.id, rg.name, rgg.geolevel_id, rg.simple as geom FROM redistricting_geounit rg JOIN redistricting_geounit_geolevel rgg ON rg.id = rgg.geounit_id WHERE rgg.geolevel_id = %%(geolevel_id)s;" % geolevel.name
         cursor.execute(sql, {'geolevel_id':geolevel.id})
-        transaction.commit()
+        try:
+            transaction.commit()
+        except:
+            transaction.rollback()
+            logger.error('Failed to create simple_%s view', geolevel.name)
+            logger.error(format_exc())
 
         logger.debug('Created simple_%s view ...', geolevel.name)
         
         for subject in Subject.objects.all():
             sql = "CREATE OR REPLACE VIEW %s AS SELECT rg.id, rg.name, rgg.geolevel_id, rg.geom, rc.number, rc.percentage FROM redistricting_geounit rg JOIN redistricting_geounit_geolevel rgg ON rg.id = rgg.geounit_id JOIN redistricting_characteristic rc ON rg.id = rc.geounit_id WHERE rc.subject_id = %%(subject_id)s AND rgg.geolevel_id = %%(geolevel_id)s;" % get_featuretype_name(geolevel.name, subject.name)
             cursor.execute(sql, {'subject_id':subject.id, 'geolevel_id':geolevel.id})
-            transaction.commit()
+            try:
+                transaction.commit()
+            except:
+                transaction.rollback()
+                logger.error('Failed to create %s view',
+                    get_featuretype_name(geolevel.name, subject.name))
+                logger.error(format_exc())
 
             logger.debug('Created %s view ...', get_featuretype_name(geolevel.name, subject.name))
 
