67e98c90e58af5e34bf46af69a555c0bde6dc2da
Kenny Shepard <kshepard@gmail.com>
2013-06-21 10:37:12 -0400

    Set DNS to localhost to fix shapefile problem

--- scripts/site-dns.sh
+++ scripts/site-dns.sh
@@ -8,4 +8,9 @@ fi
 # Set the 'site' URL to the instance DNS name
 DNS=$(curl http://169.254.169.254/latest/meta-data/local-hostname/)
 
+# If DNS is set to the site, this results in problems when spawning new AMIs
+# An AMI startup script to set this would be preferable, but also, setting it 
+# to 'localhost' seems to work, as long as it's self-contained.
+DNS=localhost
+
 su postgres -c "psql -d publicmapping -c \"update publicmapping.django_site set domain='$DNS', name='$DNS' where id=1;\""
