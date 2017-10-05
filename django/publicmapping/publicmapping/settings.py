e0b100abdeb18debb0cdb17706b71d93f419e9c7
James <james.santucci@gmail.com>
2017-10-05 10:23:16 -0400

    Hide the secret key

--- django/publicmapping/publicmapping/settings.py
+++ django/publicmapping/publicmapping/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1%a60e98ywm#$gl4mcm%s#uxoppc3)t%w+an*gzmujo-v-i07^'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
