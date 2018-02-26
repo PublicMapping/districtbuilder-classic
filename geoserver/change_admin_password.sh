08417d059c58c71f80fcb7203604457c66ffeb86
Peter Caisse <pcaisse@gmail.com>
2018-02-26 14:43:53 -0500

    Add retrying API call to change password

--- geoserver/change_admin_password.sh
+++ geoserver/change_admin_password.sh
@@ -9,9 +9,26 @@
 echo "Attempting to set a new admin password..."
 echo "Note that this assumes the current password is the default ('geoserver')"
 
-curl -X PUT http://localhost:${WEB_APP_PORT}/geoserver/rest/security/self/password \
-  -u admin:geoserver \
-  -H "accept: application/json" \
-  -H "content-type: application/json" \
-  -d "{\"newPassword\": \"${MAP_SERVER_ADMIN_PASSWORD}\"}"
+# Sometimes geoserver is not ready to receive API calls for whatever reason.
+# Retrying seems to do the trick.
+n=0
+until [ $n -ge 5 ]
+do
+  curl --silent -X PUT http://localhost:${WEB_APP_PORT}/geoserver/rest/security/self/password \
+    -u admin:geoserver \
+    -H "accept: application/json" \
+    -H "content-type: application/json" \
+    -d "{\"newPassword\": \"${MAP_SERVER_ADMIN_PASSWORD}\"}"
 
+  if [ 0 -eq $? ]; then
+    echo 'Request to update password made successfully!'
+    exit 0
+  else
+    echo 'Request to update password failed. Retrying...'
+    n=$[$n+1]
+    sleep 10
+  fi;
+done
+
+echo 'Request to update password failed permanently.'
+exit 1
