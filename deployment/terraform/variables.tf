105274239b0d51855437cb673f4ce2c9e749dc90
Taylor Nation <tnation14@gmail.com>
2018-07-09 17:02:00 -0400

    Remove unnecessary variables

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -21,23 +21,3 @@ variable "ssl_certificate_arn" {}
 variable "cdn_price_class" {
   default = "PriceClass_100"
 }
-
-variable "districtbuilder_database_name" {}
-
-variable "districtbuilder_web_app_password" {}
-
-variable "districtbuilder_admin_user" {}
-
-variable "districtbuilder_admin_email" {}
-
-variable "districtbuilder_admin_password" {}
-
-variable "districtbuilder_redis_password" {}
-
-variable "districtbuilder_geoserver_password" {}
-
-variable "districtbuilder_mailer_host" {}
-
-variable "districtbuilder_mailer_user" {}
-
-variable "districtbuilder_mailer_password" {}
