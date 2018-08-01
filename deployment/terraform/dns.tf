951bfedd6993263afad72e7d20d621817aa65d92
Taylor Nation <tnation14@gmail.com>
2018-08-01 15:14:47 -0400

    Allow domain root or subdomain for DNS
    Users can add records for example.com or
    state-name.example.com.

--- deployment/terraform/dns.tf
+++ deployment/terraform/dns.tf
@@ -5,9 +5,12 @@ data "aws_route53_zone" "external" {
   zone_id = "${data.terraform_remote_state.core.public_hosted_zone_id}"
 }
 
+locals {
+  application_domain = "${var.is_subdomain ? "${lower(var.state)}.${replace(data.aws_route53_zone.external.name, "/.$/", "")}" : replace(data.aws_route53_zone.external.name, "/.$/", "")}"
+}
 resource "aws_route53_record" "origin" {
   zone_id = "${data.aws_route53_zone.external.zone_id}"
-  name    = "origin.${lower(var.state)}.${data.aws_route53_zone.external.name}"
+  name    = "origin.${local.application_domain}"
   type    = "CNAME"
   ttl     = "300"
   records = ["${aws_instance.app_server.public_dns}"]
@@ -15,7 +18,7 @@ resource "aws_route53_record" "origin" {
 
 resource "aws_route53_record" "app" {
   zone_id = "${data.aws_route53_zone.external.zone_id}"
-  name    = "${lower(var.state)}.${data.aws_route53_zone.external.name}"
+  name    = "${local.application_domain}"
   type    = "A"
 
   alias {
