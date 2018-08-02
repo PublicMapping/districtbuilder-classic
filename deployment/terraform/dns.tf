0904371847e47ac1d07e14eba5badff019a3190e
Taylor Nation <tnation14@gmail.com>
2018-08-02 09:57:08 -0400

    Add internal record for app server

--- deployment/terraform/dns.tf
+++ deployment/terraform/dns.tf
@@ -1,13 +1,32 @@
 #
+# Private DNS resources
+#
+
+data "aws_route53_zone" "internal" {
+  zone_id = "${data.terraform_remote_state.core.private_hosted_zone_id}"
+}
+
+resource "aws_route53_record" "origin_internal" {
+  zone_id = "${data.aws_route53_zone.internal.zone_id}"
+  name    = "${lower(var.state)}.${replace(data.aws_route53_zone.internal.name, "/.$/", "")}"
+  type    = "CNAME"
+  ttl     = "10"
+  records = ["${aws_instance.app_server.private_dns}"]
+}
+
+#
 # Public DNS resources
 #
 data "aws_route53_zone" "external" {
   zone_id = "${data.terraform_remote_state.core.public_hosted_zone_id}"
 }
 
+# This allows us to place the app at either the root domain or a subdomain
+
 locals {
   application_domain = "${var.is_subdomain ? "${lower(var.state)}.${replace(data.aws_route53_zone.external.name, "/.$/", "")}" : replace(data.aws_route53_zone.external.name, "/.$/", "")}"
 }
+
 resource "aws_route53_record" "origin" {
   zone_id = "${data.aws_route53_zone.external.zone_id}"
   name    = "origin.${local.application_domain}"
