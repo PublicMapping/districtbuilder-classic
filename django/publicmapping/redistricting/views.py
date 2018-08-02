faea19cffb5b0d3494ada5e3d59b24f01a95e985
Shreshth Khilani <shreshth.khilani@gmail.com>
2018-08-02 12:24:35 -0400

    Fix timestamp on draw page and make it show local time

--- django/publicmapping/redistricting/views.py
+++ django/publicmapping/redistricting/views.py
@@ -437,7 +437,6 @@ def commonplan(request, planid):
     plan = Plan.objects.filter(id=planid)
     if plan.count() == 1:
         plan = plan[0]
-        plan.edited = getutc(plan.edited)
         levels = plan.legislative_body.get_geolevels()
         districts = plan.get_districts_at_version(
             plan.version, include_geom=False)
@@ -1179,7 +1178,7 @@ def newdistrict(request, planid):
                 status['success'] = True
                 status['message'] = _('Created 1 new district')
                 plan = Plan.objects.get(pk=planid, owner=request.user)
-                status['edited'] = getutc(plan.edited).isoformat()
+                status['edited'] = plan.edited.isoformat()
                 status['district_id'] = district_id
                 status['version'] = plan.version
             except ValidationError:
@@ -1641,7 +1640,7 @@ def addtodistrict(request, planid, districtid):
                 % {'num_fixed_districts': fixed}
             status['updated'] = fixed
             plan = Plan.objects.get(pk=planid, owner=request.user)
-            status['edited'] = getutc(plan.edited).isoformat()
+            status['edited'] = plan.edited.isoformat()
             status['version'] = plan.version
         except Exception, ex:
             status['exception'] = traceback.format_exc()
